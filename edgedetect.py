d9afd7d19eb1e6ac811e9f3e8ac9efb293112c19
Krishnansriram122000 <39411486+Krishnansriram122000@users.noreply.github.com>
2019-07-25 22:11:54 +0530

    updated convolution and pooling process

--- edgedetect.py
+++ edgedetect.py
@@ -1,40 +1,51 @@
-# USAGE
-# open cmd
-# type cd c:\python\python37-32\src
-# then type
-# python edgedetect.py -i ner.png
-
-# import the necessary packages
 import argparse
 import cv2
 import numpy  as np
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-i", "--image", help="Path to the image")
 args = vars(ap.parse_args())
 
+if not args.get("image", False):
+	camera = cv2.VideoCapture(0)
+	while True:
+		(return_value, image)=camera.read()
+		cv2.imshow("camera",image)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("c") :
+			break
+	camera.release()
+	cv2.destroyAllWindows()
+ 
 # load the image and show some basic information on it
-image = cv2.imread(args["image"])
+if  args.get("image", False):
+	image = cv2.imread(args["image"])
 (h,w)=image.shape[:2]
+#
+threshold=int(input('Enter the threshold : '))
 print("width: {w} pixels".format(w=image.shape[1]))
 print("height: {h}  pixels".format(h=image.shape[0]))
 print("channels: {c}".format(c=image.shape[2]))
 #you could even blur the image before this using the commands
-#blurred = cv2.GaussianBlur(image, (15,15), 0)
-#cv2.imshow("blur",blurred)
+blurred = cv2.GaussianBlur(image, (15,15), 0)
+cv2.imshow("blur",blurred)
 f=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-k=np.int32([[-1,-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1,-1],[0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1,1]])
-
-yy=np.int32([[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1]])
+k=np.float32([[0,0,0,0,-0.25,0,0,0,0],[0,0,0,-0.25,-0.5,-0.25,0,0,0],[0,0,-0.25,-0.5,-0.75,-0.5,-0.25,0,0],[0,-0.25,-0.5,-0.75,-1,-0.75,-0.5,-0.25,0],[0,0,0,0,0,0,0,0,0],[0,0.25,0.5,0.75,1,0.75,0.5,0.25,0],[0,0,0.25,0.5,0.75,0.5,0.25,0,0],[0,0,0,0.25,0.5,0.25,0,0,0],[0,0,0,0,0.25,0,0,0,0]])
+k1=np.float32([[0,0,0,-0.125,0,0.125,0,0,0],[0,0,-0.125,-0.25,0,0.25,0.125,0,0],[0,0,-0.25,-0.5,0,0.5,0.25,0,0],[0,-0.25,-0.5,-0.75,0,0.75,0.5,0.25,0],[-0.25,-0.5,-0.75,-1,0,1,0.75,0.5,0.25],[0,-0.25,-0.5,-0.75,0,0.75,0.5,0.25,0],[0,0,-0.25,-0.5,0,0.5,0.25,0,0],[0,0,-0.125,-0.25,0,0.25,0.125,0,0],[0,0,0,-0.125,0,0.125,0,0,0]])
+#yy=np.float32([[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1],[-1,-1,-1,-1,0,1,1,1,1]])
 imag2=np.zeros(image.shape[:2],dtype="uint8")
 imag3=np.zeros(image.shape[:2],dtype="uint8")
+imag4=np.zeros([int(h/2),int(w/2)],dtype="uint8")
+imag5=np.zeros([int(h/2),int(w/2)],dtype="uint8")
 sum=0
 cv2.imshow("original",image)
 cv2.waitKey(0)
+print (k)
+print(k1)
 
-for i in range(0,h-9,9):
-	for j in range(0,w-9,9):
+for i in range(0,h-9):
+	for j in range(0,w-9):
 		for m in range(9):
 			for n in range(9):
 
@@ -43,33 +54,50 @@ for i in range(0,h-9,9):
 
 					sum=sum+(u*y)
 		#print(sum)	
-		for m in range(9):
-				imag2[i+4,j+m]=sum
+		#for m in range(9):
+		if sum>threshold :
+			imag2[i+4,j+4]=sum
 		sum=0
-for i in range(0,h-9,9):
-	for j in range(0,w-9,9):
+cv2.imshow("Horizontaledges",imag2)
+cv2.waitKey(0)
+for i in range(0,h-9):
+	for j in range(0,w-9):
 		for m in range(9):
 			for n in range(9):
 
-					y=int(yy[m][n])
+					y=int(k1[m][n])
 					u=int(f[i+m,j+n])
 
 					sum=sum+(u*y)
 		#print(sum)
-		for m in range(9):
-				imag3[i+m,j+4]=sum
+		#for m in range(9):
+		if sum>threshold :
+			imag3[i+4,j+4]=sum
 		sum=0
-cv2.imshow("Verticaledges",imag2)
+
+for i in range(0,int(h)-2,2):
+	for j in range(0,int(w)-2,2):
+		list1=[int(imag2[i,j]),int(imag2[i+1,j]),int(imag2[i,j+1]),int(imag2[i+1,j+1])]
+		list1.sort()
+		imag4[int(i/2),int(j/2)]=int(list1[-1])
+for i in range(0,int(h)-2,2):
+	for j in range(0,int(w)-2,2):
+		list1=[int(imag3[i,j]),int(imag3[i+1,j]),int(imag3[i,j+1]),int(imag3[i+1,j+1])]
+		list1.sort()
+		imag5[int(i/2),int(j/2)]=list1[-1]
+cv2.imshow("pooling1",imag4)
 cv2.waitKey(0)
-cv2.imshow("horizontaledges",imag3)
+cv2.imshow("pooling2",imag5)
+cv2.waitKey(0)
+cv2.imshow("verticaledges",imag3)
 cv2.waitKey(0)
 krish=cv2.add(imag3,imag2)
 cv2.imshow("both",krish)
 cv2.waitKey(0)
-#gh=cv2.bitwise_not(imag2)
-#uip=cv2.bitwise_and(gh,gh,mask=f)
+gh=cv2.bitwise_not(imag2)
+uip=cv2.bitwise_and(gh,gh,mask=f)
 # show the image and wait for a keypress
-#cv2.imshow("Image",uip)
+cv2.imshow("Image",uip)
 cv2.waitKey(0)
 
 # save the image -- OpenCV handles converting filetypes
